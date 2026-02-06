677614c7ff41937dab7db0e540b1ca97727c2cd2
Renan Caldas <renan@renancaldas.dev.br>
2026-02-05 21:48:12 -0300

    feat: pre-commit e pre-push

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
 test('the application returns a successful response', function () {
-    $response = $this->get('/health');
+    $response = $this->get('/api/health');
 
     $response->assertStatus(200);
 });
